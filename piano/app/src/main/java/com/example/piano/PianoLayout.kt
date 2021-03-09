a8a926b84acc334f8d9c440282b13c041ab521a3
Marius Beurling <maribe16@uia.no>
2021-03-09 18:03:06 +0100

    Added piano2

--- piano/app/src/main/java/com/example/piano/PianoLayout.kt
+++ piano/app/src/main/java/com/example/piano/PianoLayout.kt
@@ -3,18 +3,25 @@ package com.example.piano
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Chronometer
+import android.widget.Toast
+import com.example.piano.data.Note
 import com.example.piano.databinding.FragmentPianoBinding
+import kotlinx.android.synthetic.main.fragment_piano.*
 import kotlinx.android.synthetic.main.fragment_piano.view.*
+import java.io.File
+import java.io.FileOutputStream
 
 class PianoLayout : Fragment() {
     private var _binding:FragmentPianoBinding? = null
     private val binding get() = _binding!!
 
     private val fullTones = listOf("C","D","E","F","G","A","B","C2","D2","E2","F2","G2")
-    private val halfTones = listOf("C#","D#","E#","F#","G#","A#","B#","C2#","D2#","E2#","F2#","G2#")
-
+    private val halfTones = listOf("C#","D#","F#","G#","A#","C2#","D2#","F2#")
+    private var score:MutableList<Note> = mutableListOf<Note>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -29,11 +36,17 @@ class PianoLayout : Fragment() {
         fullTones.forEach {
             val fullTonePianoKey = FullTonePianoKeyFragment.newInstance(it)
 
-            fullTonePianoKey.onKeyDown = {
-                println("Piano key down $it")
+            var startPlay:Long = 0
+
+            fullTonePianoKey.onKeyDown = { note ->
+                startPlay = System.nanoTime()
+                println("Piano key down $note")
             }
             fullTonePianoKey.onKeyUp ={
-                println("Piano key up $it")
+                var endPlay = System.nanoTime()
+                val note = Note(it, startPlay,endPlay)
+                score.add(note)
+                println("Piano key up $note")
             }
 
             ft.add(view.pianoKeyslayout.id,fullTonePianoKey,"note_$it")
@@ -42,12 +55,15 @@ class PianoLayout : Fragment() {
 
         halfTones.forEach {
             val halfTonePianoKey = HalfTonePianoKeyFragment.newInstance(it)
-
-            halfTonePianoKey.onKeyDown = {
-                println("Piano key down $it")
+            var startPlay:Long = 0
+            halfTonePianoKey.onKeyDown = { note ->
+                startPlay = System.nanoTime()
+                println("Piano key down $note")
             }
-            halfTonePianoKey.onKeyUp ={
-                println("Piano key up $it")
+            halfTonePianoKey.onKeyUp ={ note ->
+                var endPlay = System.nanoTime()
+                val note = Note(it,startPlay,endPlay)
+                println("Piano key up $note")
             }
 
             ft.add(view.pianobKeyslayout.id,halfTonePianoKey,"note_$it")
@@ -56,7 +72,37 @@ class PianoLayout : Fragment() {
 
         ft.commit()
 
-        return inflater.inflate(R.layout.fragment_piano, container, false)
+        view.saveScoreBt.setOnClickListener{
+
+            var fileName =view.fileNameTextEdit.text.toString()
+            val path = this.activity?.getExternalFilesDir(null)
+            val newFile = (File(path, fileName))
+
+            when {
+                score.count() == 0 -> Toast.makeText(activity, "Nothing to save", Toast.LENGTH_SHORT).show()
+                fileName.isEmpty() -> Toast.makeText(activity, "No Name", Toast.LENGTH_SHORT).show()
+                path == null -> Toast.makeText(activity, "No path", Toast.LENGTH_SHORT).show()
+                newFile.exists() -> Toast.makeText(activity, "Already a file with that name", Toast.LENGTH_SHORT).show()
+
+                else -> {
+                    fileName = "$fileName.musikk"
+                    FileOutputStream(newFile, true).bufferedWriter().use{ writer ->
+                        score.forEach {
+                            writer.write("${it.toString()}\n")
+                    }
+                    FileOutputStream(newFile).close()
+                 }
+                    score.clear()
+
+                }
+            }
+
+        }
+
+
+
+
+        return view
     }
 
 
